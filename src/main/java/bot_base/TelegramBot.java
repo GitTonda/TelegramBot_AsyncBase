61ce20379c7bfbb0e7018247326397900a0323e7
Samuele Tonda Roc <samuele.tondaroc@edu.unito.it>
2025-10-01 14:46:23 +0200

    modified executor service to be fixed (50 by default); added LinkedBlockingQueue<Update> to handle bursts of updates; also added thread handler that waits for elements in the queue.

--- src/main/java/bot_base/TelegramBot.java
+++ src/main/java/bot_base/TelegramBot.java
@@ -3,19 +3,35 @@ package bot_base;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class TelegramBot extends TelegramLongPollingBot
 {
+    private static final int THREAD_CAP = 50, QUEUE_CAP = 1000;
     String bot_username;
     ExecutorService executor;
+    LinkedBlockingQueue<Update> updates_queue;
 
     public TelegramBot(String bot_username, String bot_token)
     {
         super(bot_token);
         this.bot_username = bot_username;
-        executor = Executors.newCachedThreadPool();
+        executor = Executors.newFixedThreadPool(THREAD_CAP);
+        updates_queue = new LinkedBlockingQueue<>(QUEUE_CAP);
+
+        // Preparing Threads
+        executor.submit(() ->
+        {
+            while (!Thread.currentThread().isInterrupted())
+            {
+                try
+                {
+                    handle_updates(updates_queue.take());
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt(); // set flag again
+                }
+            }
+        });
     }
 
     @Override
@@ -25,9 +41,12 @@ public class TelegramBot extends TelegramLongPollingBot
     }
 
     @Override
-    public void onUpdateReceived(Update update)
-    {
-        executor.submit(() -> handle_updates(update));
+    public void onUpdateReceived(Update update) {
+        try {
+            updates_queue.put(update);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 
     private void handle_updates (Update update)
